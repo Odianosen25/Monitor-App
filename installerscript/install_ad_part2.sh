d47ed860f23ea632975955b1a41450b78b09bf38
TheStigh <43895442+TheStigh@users.noreply.github.com>
2020-02-17 23:00:38 +0100

    Fixed paths and removed need for user pi

--- installerscript/install_ad_part2.sh
+++ installerscript/install_ad_part2.sh
@@ -68,7 +68,7 @@ echo -e "\e[32m[STEP 7/10] Createing folders | Done\e[0m"
 
 # Copy remainig files to correct folders
 echo -e "\e[96m[STEP 8/10] Copy configuration files and Monitor-App to AppDaemon...\e[90m"
-if cp /home/pi/Monitor-App/installerscript/appdaemon.yaml /home/appdaemon/.appdaemon/conf/appdaemon.conf;
+if ~/installerscript/appdaemon.yaml /home/appdaemon/.appdaemon/conf/appdaemon.conf;
 then
     echo -e "\e[32m Copy configuration files | Done\e[0m"
 else
@@ -76,7 +76,7 @@ else
     exit;
 fi
 
-if cp /home/pi/Monitor-App/installerscript/apps.yaml /home/appdaemon/.appdaemon/conf/apps/apps.yaml;
+if cp ~/installerscript/apps.yaml /home/appdaemon/.appdaemon/conf/apps/apps.yaml;
 then
     echo -e "\e[32m Copy Monitor-App to AppDaemon | Done\e[0m"
 else
@@ -84,7 +84,7 @@ else
     exit;
 fi
 
-if cp /home/pi/Monitor-App/apps/home_presence_app/home_presence_app.py /home/appdaemon/.appdaemon/conf/apps/home_presence_app/home_presence_app.py;
+if cp ~/installerscript/home_presence_app.py /home/appdaemon/.appdaemon/conf/apps/home_presence_app/home_presence_app.py;
 then
     echo -e "\e[32m[STEP 8/10] Copy final files | Done\e[0m"
 else

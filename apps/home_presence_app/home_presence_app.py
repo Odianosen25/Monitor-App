a97b130543a2774a84bcfbfddd5b60d15f5752f4
Odianosen Ejale <mariatony.ejale@gmail.com>
2020-02-23 19:58:48 +0000

    Bumped version to 2.2.3

--- apps/home_presence_app/home_presence_app.py
+++ apps/home_presence_app/home_presence_app.py
@@ -22,7 +22,7 @@ import copy
 from datetime import datetime, timedelta
 
 
-__VERSION__ = "2.2.2"
+__VERSION__ = "2.2.3"
 
 # pylint: disable=attribute-defined-outside-init,unused-argument
 class HomePresenceApp(ad.ADBase):

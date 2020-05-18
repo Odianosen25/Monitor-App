bbfa0b521c6434e274a391881095af79fec6aee7
Odianosen Ejale <mariatony.ejale@gmail.com>
2020-05-18 18:23:07 +0100

    Bump to 2.3.4

--- apps/home_presence_app/home_presence_app.py
+++ apps/home_presence_app/home_presence_app.py
@@ -23,7 +23,7 @@ from datetime import datetime, timedelta
 import traceback
 
 
-__VERSION__ = "2.3.3"
+__VERSION__ = "2.3.4"
 
 # pylint: disable=attribute-defined-outside-init,unused-argument
 class HomePresenceApp(ad.ADBase):
